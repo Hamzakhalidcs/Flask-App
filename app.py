e1964aaf9f10de2f611ca1486da8df647824215b
Hamzakhalidcs <hamzakhalidcs007@gmail.com>
2021-02-23 19:48:30 +0500

    initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask,request,jsonify
+
+app =Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World 2!"
+
+@app.route('/sum/<num1>/<num2>', methods=['GET'])
+def create_cm(num1, num2):
+    try:
+        if num1 and num2:
+            n1 = int(num1)
+            n2 = int(num2)
+            return jsonify({"result": n1 + n2})
+
+        return jsonify({"error" : str(e), "message": "args not provided"})
+        
+    except Exception as e:
+        print ("Missing args")
+        return jsonify({"error" : str(e), "message": "args not provided"})
+
+
+if __name__ =="__main__":
+    app.run()
